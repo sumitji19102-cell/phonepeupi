25854d41363277d1a4e2abc7e8112d29ec5447fc
aneeshmaury <aneeshmaury.in@gmail.com>
2026-05-06 17:08:43 +0530

    rturyu

--- app/page.js
+++ app/page.js
@@ -140,7 +140,7 @@ export default function Page() {
 
             <img
               src="https://i.ibb.co/dwqWqWTP/Chat-GPT-Image-May-1-2026-12-00-20-PM-removebg-preview.png"
-              className="w-[65px] ml-"
+              className="w-[70px] ml-"
             />
 
             <span className="mr-1">•</span>
